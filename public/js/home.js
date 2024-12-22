49200666c97aaccd5e2b37551a81820d07340e94
Dom Kirby <dom@domkirby.com>
2024-12-21 21:04:46 -0700

    Fixed frontend error handler; removed unnecessary console traffic

--- public/js/home.js
+++ public/js/home.js
@@ -8,36 +8,32 @@ $(document).ready(function() {
         const plaintext = $("#secret").val();
         try {
             const encryptedData = await encryptData(key, plaintext);
+            $("#encryptedSecret").val(encryptedData);
+                var payload = form.serialize();
+                
+                $.ajax({
+                    method: "POST",
+                    url: "/api/saveSecret",
+                    data: payload,
+                    success: function(data) {
+                        var secretUrl = `https://${windowHost}/secret/${data.secret_id}#${key}`;
+                        $("#secretLink").val(secretUrl);
+                        $("#secretLinkContainer").show();
+                        $("#createSecretContainer").hide();
+                        $("#copyLink").on("click", function() {
+                            navigator.clipboard.writeText(secretUrl);
+                            $("#copyLink").text("Copied!");
+                        });
+                    },
+                    error: function($xhr) {
+                        $("#errors").text($xhr.responseJSON.error);
+                        $("#errors").show();
+                    }
+            });
         } catch(error) {
             console.error("Error encrypting data:", error);
             $("#errors").text("Error encrypting data. Please refresh and retry.");
             $("#errors").show();
         }
-        
-        
-        $("#encryptedSecret").val(encryptedData);
-        var payload = form.serialize();
-        console.log("encryptedData:", encryptedData);
-        console.log("Payload:", payload);
-        
-        $.ajax({
-            method: "POST",
-            url: "/api/saveSecret",
-            data: payload,
-            success: function(data) {
-                var secretUrl = `https://${windowHost}/secret/${data.secret_id}#${key}`;
-                $("#secretLink").val(secretUrl);
-                $("#secretLinkContainer").show();
-                $("#createSecretContainer").hide();
-                $("#copyLink").on("click", function() {
-                    navigator.clipboard.writeText(secretUrl);
-                    $("#copyLink").text("Copied!");
-                });
-            },
-            error: function($xhr) {
-                $("#errors").text($xhr.responseJSON.error);
-                $("#errors").show();
-            }
-        });
     });
 });
